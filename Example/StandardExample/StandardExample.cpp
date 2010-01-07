b93afa8b7f3acd0ce82aee27638292a8561eb259
miro <miroslav.xsedlak@gmail.com>
2010-01-07 23:03:00 +0100

    added basic tutorial, some other changes in dir. tree

---
+++ Example/StandardExample/StandardExample.cpp
@@ -0,0 +1,35 @@
+// source: http://www.evocomp.de/tutorials/tutorium_cppunit/howto_tutorial_cppunit_en.html
+
+#ifndef FRACTIONTEST_H
+#define FRACTIONTEST_H
+
+#include <cppunit/TestFixture.h>
+#include <cppunit/extensions/HelperMacros.h>
+#include "StandardExample.h"
+
+using namespace std;
+
+class fractiontest : public CPPUNIT_NS :: TestFixture
+{
+    CPPUNIT_TEST_SUITE (fractiontest);
+    CPPUNIT_TEST (addTest);
+    CPPUNIT_TEST (subTest);
+    CPPUNIT_TEST (exceptionTest);
+    CPPUNIT_TEST (equalTest);
+    CPPUNIT_TEST_SUITE_END ();
+
+    public:
+        void setUp (void);
+        void tearDown (void);
+
+    protected:
+        void addTest (void);
+        void subTest (void);
+        void exceptionTest (void);
+        void equalTest (void);
+
+    private:
+        Fraction *a, *b, *c, *d, *e, *f, *g, *h;
+};
+
+#endif
