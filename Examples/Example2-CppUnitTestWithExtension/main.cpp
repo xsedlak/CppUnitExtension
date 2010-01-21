944026e8b1c294579487b2f29c727f2c7d81b3ee
miro <miroslav.xsedlak@gmail.com>
2010-01-21 01:43:17 +0100

    interface between UTE and CppUnit is created

--- Examples/Example2-CppUnitTestWithExtension/main.cpp
+++ Examples/Example2-CppUnitTestWithExtension/main.cpp
@@ -3,33 +3,37 @@
 #include <cppunit/TestResult.h>
 #include <cppunit/TestResultCollector.h>
 #include <cppunit/BriefTestProgressListener.h>
-//#include <cppunit/TestRunner.h>
-#include <CueTestRunner.h>
+#include <TestClass.h>
+#include <UteTestRunner.h>
+
+using namespace std;
+using namespace ute;
 
 int main (int argc, char* argv[])
 {
-	// informs test-listener about testresults
-	CPPUNIT_NS :: TestResult testresult;
+  // informs test-listener about testresults
+  CPPUNIT_NS :: TestResult testresult;
 
-	// register listener for collecting the test-results
-	CPPUNIT_NS :: TestResultCollector collectedresults;
-	testresult.addListener (&collectedresults);
+  // register listener for collecting the test-results
+  CPPUNIT_NS :: TestResultCollector collectedresults;
+  //	testresult.addListener (&collectedresults);
 
-	// register listener for per-test progress output
-	CPPUNIT_NS :: BriefTestProgressListener progress;
-	testresult.addListener (&progress);
+  // register listener for per-test progress output
+  	CPPUNIT_NS :: BriefTestProgressListener progress;
+  	testresult.addListener (&progress);
 
-	// insert test-suite at test-runner by registry
-	// (cppunit testrunner is replaced by new one CueTestRunner which will execute tests in diff. way)
-	cue::CueTestRunner testrunner;
-	testrunner.run (testresult);
+  // insert test-suite at test-runner by registry
+  // (cppunit testrunner is replaced by new one UteTestRunner which will execute tests in diff. way)
+  ute::UteTestRunner testrunner;
+  utr()->utr_store_test_class(new TestClass("BaseTest",NULL));
+  testrunner.run (testresult, argc, argv);
 
-	// output results in compiler-format
-	CPPUNIT_NS :: CompilerOutputter compileroutputter (&collectedresults, std::cerr);
-	compileroutputter.write ();
+  // output results in compiler-format
+  CPPUNIT_NS :: CompilerOutputter compileroutputter (&collectedresults, std::cerr);
+//  compileroutputter.write ();
 
-	// return 0 if tests were successful
-	return collectedresults.wasSuccessful () ? 0 : 1;
+  // return 0 if tests were successful
+  return collectedresults.wasSuccessful () ? 0 : 1;
 }
 
-// vim: ts=4 sw=4 tw=0 noet
+// vim: ts=2 sw=2 et
